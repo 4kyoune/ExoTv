9f2d4ee63ac4acc1af8fcf579d94a0c9a9375844
4kyoune <wangyyputra@gmail.com>
2024-05-23 00:04:07 +0700

    Update [...params].tsx

--- src/pages/anime/details/[...params].tsx
+++ src/pages/anime/details/[...params].tsx
@@ -33,105 +33,170 @@ import { useEffect, useMemo } from "react";
 import { isMobile } from "react-device-detect";
 import { BsFillPlayFill } from "react-icons/bs";
 
-async function getInfo(id) {
-  try {
-    let cachedData;
-    if (redis) {
-      cachedData = await redis.get(`info:${id}`);
-      if (!JSON.parse(cachedData)) {
-        await redis.del(`info:${id}`);
-        cachedData = null;
-      }
+type Props = {
+    params: {
+        streamid: string;
+        animeid: number;
+    };
+};
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    try {
+        const anime = await getAnimeDetails(params.animeid);
+        if (anime) {
+            const description = anime.description;
+            const formattedDescription = description?.replace(/<\/?[^>]+(>|$)/g, "");
+            return {
+                title: `${`Watching ${params.streamid} on AnimeTrix` || "Opps!! No Title Found"} On AnimeTrix Watch Or Download For Free`,
+                description: formattedDescription || "Opps!! No Description Found",
+                openGraph: {
+                    images: anime?.cover || "https://cdn.discordapp.com/attachments/1079039236302446705/1166676085883285544/animetrixbanner.jpg?ex=654b5ac6&is=6538e5c6&hm=6d9c8c991b0897a33364a58aeea177e53c26216c617b6dff9b5de7607b9bf332&",
+                },
+            };
+        } else {
+            throw new Error("Anime details are missing or incomplete");
+        }
+    } catch (error) {
+        console.error("Error fetching anime details:", error);
+        return {
+            title: "Error",
+            description: "Oops! An error occurred while fetching anime details.",
+            openGraph: {
+                images: "https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+        };
     }
-    if (cachedData) {
-      return JSON.parse(cachedData);
+}
+const Page = async ({
+    params,
+}: {
+    params: {
+        streamid: string;
+        animeid: number;
+    };
+}) => {
+    const details = await getAnimeDetails(params.animeid);
+    const stream = await getSteamingLink(params.streamid);
+    const download = await getDownloadLink(params.streamid);
+    const getAbbreviatedMonth = (month: number): string => {
+        const abbreviatedMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        return abbreviatedMonths[month];
+    };
+    // Get the next airing date for the anime.
+    const airingDate = new Date(details.nextAiringEpisode?.airingTime * 1000);
+
+    // Extract the day, month, and year from the airing date.
+    const day = airingDate.getDate();
+    const month = getAbbreviatedMonth(airingDate.getMonth());
+    const year = airingDate.getFullYear();
+    const currentYear = new Date().getFullYear();
+
+    // Format the time string using the 12-hour clock format.
+    const options: Intl.DateTimeFormatOptions = {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+    const timeString = airingDate.toLocaleString(undefined, options);
+
+    // Format the airing date string based on the year.
+    let formattedAiringDate;
+    if (year === currentYear) {
+        formattedAiringDate = `${day} ${month} at ${timeString}`;
     } else {
-      const data = await AnimeInfoAnilist(id);
-      const cacheTime = data?.nextAiringEpisode?.episode ? 60 * 60 * 2 : 60 * 60 * 24 * 45;
-      if (redis && data !== null && data) {
-        await redis.set(`info:${id}`, JSON.stringify(data), "EX", cacheTime);
-      }
-      return data;
+        formattedAiringDate = `${day} ${month} ${year} at ${timeString}`;
     }
-  } catch (error) {
-    console.error("Error fetching info: ", error);
-  }
-}
+    return (
+        <>
+            {Object.keys(details || stream).length > 0 ? (
+                <section className="p-2 min-h-screen">
+                    <Suspense fallback={<></>}>
+                        <AddToHistory streamId={params.streamid} animeId={params.animeid} title={stream?.info?.title || "unknown"} episode={stream?.info?.episode || "unknown"} coverImage={details?.cover || "unknown"} image={details?.image || "Unknown"} />
+                    </Suspense>
+                    <div className=" w-full">
+                        <div className=" flex justify-between lg:flex-row flex-col gap-5">
+                            <div className="flex flex-col w-full 2xl:w-[70vw]">
+                                <iframe src={stream?.plyr?.main || stream?.plyr?.backup} scrolling="no" frameBorder="0" allowFullScreen={true} title={params.streamid} allow="picture-in-picture" className="w-full rounded-lg h-[30vh] lg:h-[50vh] md:h-[30vh]"></iframe>
+                                <div className="mt-2">
+                                    <h1 className="font-semibold text-2xl lg:text-4xl text-white">{stream?.info?.title || "unknown"}</h1>
+                                    <span className="text-lg mt-4 lg:text-2xl font-semibold text-white/70">Episode : {stream?.info?.episode || "unknown"}</span>
+                                </div>
 
-export async function generateMetadata({ params, searchParams }) {
-  const id =  searchParams?.id;
-  const data = await getInfo(id);
-  const epnum =  searchParams?.ep;
+                                <div className="flex gap-3 mt-6 flex-wrap items-center">
+                                    <a href={download?.download || "https://www.youtube.com/watch?v=dQw4w9WgXcQ"} target="_blank" className=" bg-white  p-2 rounded-lg font-semibold text-black  flex items-center gap-3" rel="noopener noreferrer">
+                                        <Download />
+                                        Download
+                                    </a>
+                                    <Share title={params.streamid} />
+                                    <a href="https://github.com/ShivaBhattacharjee/AnimeTrix-next/issues" target="_blank" className=" bg-white  p-2 rounded-lg font-semibold text-black  flex gap-3 items-center">
+                                        <Flag />
+                                        Report
+                                    </a>
+                                </div>
+                                {details.nextAiringEpisode !== undefined && <NextAiringEpisode nextAiringEpisode={details?.nextAiringEpisode?.episode} formattedAiringDate={formattedAiringDate} />}
 
-  return {
-    title:"Episode "+ epnum + ' - ' + data?.title?.english || data?.title?.romaji || 'Loading...',
-    description: data?.description?.slice(0,180),
-    openGraph: {
-      title:"Episode "+ epnum + ' - ' + data?.title?.english || data?.title?.romaji,
-      images: [data?.coverImage?.extraLarge],
-      description: data?.description,
-    },
-    twitter: {
-      card: "summary",
-      title:"Episode "+ epnum + ' - ' + data?.title?.english || data?.title?.romaji,
-      description: data?.description?.slice(0,180),
-    },
-  }
-}
+                                <div className="flex gap-3 w-full mt-5">
+                                    <img src={details?.image} alt={`an image of ${params?.streamid}`} className=" w-40 md:w-44 rounded-lg" />
+                                    <div className="flex flex-wrap w-full gap-3 text-md md:text-lg flex-col font-semibold">
+                                        <h1>
+                                            <span className="text-white/70 ">Status</span> : {details?.status || "Undefined"}
+                                        </h1>
+                                        <h1>
+                                            <span className=" text-white/70 ">Season</span> : {details?.season || "Unknown"}
+                                        </h1>
+                                        <h1>
+                                            <span className=" text-white/70 ">Audio</span> : {details?.subOrDub || "Unknown"}
+                                        </h1>
+                                        <h1>
+                                            <span className=" text-white/70">Type</span> : {details?.type || "Unknown"}
+                                        </h1>
+                                        <h1>
+                                            <span className=" text-white/70">Country</span> : {details?.countryOfOrigin || "Earth"}
+                                        </h1>
+                                    </div>
+                                    <AddToBookmark image={details?.image} animeId={params.animeid} title={stream?.info?.title || "unknown"} isStream={true} />
+                                </div>
 
-export async function Ephistory(session, aniId, epNum){
-  try {
-    let savedep;
-    if (session && aniId && epNum) {
-      await createWatchEp(aniId, epNum);
-      savedep = await getEpisode(aniId, epNum);
-    }
-    return savedep;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+                                <div className="flex flex-wrap gap-4 mt-5 text-lg font-semibold">
+                                    {details?.genres &&
+                                        Object?.keys(details?.genres).length > 0 &&
+                                        details.genres.map((genre: string, index: number) => (
+                                            <Link href={`/genres/${genre}`} className="border-2 text-sm lg:text-lg border-white  border-dotted rounded-lg p-2 duration-200 hover:border-solid" key={index}>
+                                                {genre}
+                                            </Link>
+                                        ))}
+                                </div>
+                            </div>
+                            <div className="mt-3">
+                                <Suspense fallback={<EpisodeLoading />}>
+                                    <EpisodeLists currentlyPlaying={stream?.info?.episode} animeId={params.animeid} isStream={true} />
+                                </Suspense>
+                            </div>
+                        </div>
+                    </div>
+                    <Suspense>
+                        <CommentSection streamId={params.streamid} />
+                    </Suspense>
+                    <div className="mt-7 flex flex-col gap-5">
+                        <Suspense fallback={<RelationLoading />}>
+                            <RelationCard id={params.animeid} />
+                        </Suspense>
+                    </div>
+                    <div className="mt-6">
+                        <Suspense fallback={<CharactersLoading />}>
+                            <CharacterCard characters={details.characters} />
+                        </Suspense>
+                    </div>
+                    <div className="mt-12">
+                        <Suspense fallback={<RecommendedLoading />}>
+                            <RecommendedAnime episode={params.animeid} />
+                        </Suspense>
+                    </div>
+                </section>
+            ) : (
+                <ServerError />
+            )}
+        </>
+    );
 };
 
-async function AnimeWatch({ params, searchParams }) {
-  const session = await getAuthSession();
-  const id = searchParams.id;
-  const provider = searchParams.host;
-  const epNum = searchParams.ep;
-  const epId = searchParams.epid;
-  const subdub = searchParams.type;
-  const data = await getInfo(id);
-  const savedep = await Ephistory(session, id, epNum);
-  // console.log(savedep)
-  // console.log(data)
-
-  return (
-    <>
-        <Navbarcomponent />
-      <div className=" w-full flex flex-col lg:flex-row lg:max-w-[98%] mx-auto xl:max-w-[94%] lg:gap-[6px] mt-[70px]">
-        <div className="flex-grow w-full h-full">
-          <PlayerComponent id={id} epId={epId} provider={provider} epNum={epNum} data={data} subdub={subdub} session={session} savedep={savedep}/>
-          {data?.status === 'RELEASING' &&
-            <NextAiringDate nextAiringEpisode={data?.nextAiringEpisode} />
-          }
-        </div>
-        <div className="h-full lg:flex lg:flex-col md:max-lg:w-full gap-10">
-        {/* <div className="rounded-lg hidden lg:block lg:max-w-[280px] xl:max-w-[380px] w-[100%] xl:overflow-y-scroll xl:overflow-x-hidden overflow-hidden scrollbar-hide overflow-y-hidden">
-            <PlayerAnimeCard data={data?.relations?.edges} id="Related Anime"/>
-          </div> */}
-          <div className="rounded-lg hidden lg:block lg:max-w-[280px] xl:max-w-[380px] w-[100%] xl:overflow-y-scroll xl:overflow-x-hidden overflow-hidden scrollbar-hide overflow-y-hidden">
-            <PlayerAnimeCard data={data?.recommendations?.nodes} id="Recommendations"/>
-          </div>
-        </div>
-        {/* <div className="lg:hidden">
-        <Animecards data={data?.relations?.edges} cardid="Related Anime"/>
-        </div> */}
-        {/* <div className="lg:hidden">
-        <Animecards data={data?.recommendations?.nodes} cardid={"Recommendations"}/>
-        </div> */}
-      </div>
-    </>
-  );
-}
-
-export default AnimeWatch;
+export default Page;
